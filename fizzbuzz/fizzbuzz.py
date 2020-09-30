a2bfe30f7601c6846306d22e267e6001a4d25d24
Sadia <sadia@kali.localdomain>
2020-09-30 18:35:03 -0500

    Pass all Unitest

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -2,9 +2,10 @@ import click
 import sys
 
 
-def is_positive_integer(n):
+def is_positive_integer(n): 
     """Returns True if n is a positive integer"""
-
+    if type(n) is int and n > 0: 
+        return True 
 
     return False
 
@@ -14,8 +15,14 @@ def process(n):
        - n is evenly divisible by 3 (returns 'Fizz')
        - n is evenly divisible by 5 (returns 'Buzz')
        - n is evenly divisible by both 3 and 5 (returns 'FizzBuzz')"""
-
-    return ''
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    if n % 3 == 0: 
+        return 'Fizz'
+    if n % 5 == 0:
+        return 'Buzz'
+    else:
+         return str(n)
 
 
 @click.command()
